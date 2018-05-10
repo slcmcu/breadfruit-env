b82677060b1a84fdb1f3693ab588bfe852bb8200
sunlichao <sun-lichao@163.com>
2018-05-10 10:41:52 +0800

    add project tmp

--- breadfruit.py
+++ breadfruit.py
@@ -1,24 +1,51 @@
 import sys
+import os
 def lib():
     print("create lib")
 
-def project():
-    print("create project")
+def project(pname):
+    path=pname
+    os.mkdir(pname)
+    print("create project "+pname)
+
+    path=pname+"/"+"tools"
+    os.mkdir(path)
+    print("create tools")
+
+    path=pname+"/"+"library"
+    os.mkdir(path)
+    print("create library")
+
+    path=pname+"/"+"docs"
+    os.mkdir(path)
+    print("create docs")
+    
+    path=pname+"/"+"requirements.txt"
+    f = open(path, "w")
+    f.write( "#requirements.txt \n")
+    f.close()
+    print("requirements.txt")
+
+    path=pname+"/"+"readme.txt"
+    f = open(path, "w")
+    f.write( "#readme.txt \n")
+    f.close()
+    print("readme.md")
 
 def help():
     print("hello world!")
 
 def main(cmd):
-    lcmd=len(cmd)
-    print(str(sys.argv))
-    print(len(sys.argv))
-    if lcmd==2:    
-        if cmd[1]=='startproject':
-            project()
-        if cmd[1]=='lib':
-            lib()
-        if cmd[1]=='h' or cmd[1] == 'help':
-            help()
+    #lcmd=len(cmd)
+    #print(str(sys.argv))
+    #print(len(sys.argv))   
+    if cmd[1]=='startproject':
+        pname=cmd[2]
+        project(pname)
+    if cmd[1]=='lib':
+        lib()
+    if cmd[1]=='h' or cmd[1] == 'help':
+        help()
     else:
         help()
 
